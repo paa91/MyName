411a0d57b5e465ca1e01779e85e7978df3e3b8e4
Aleksey Polucyganov <poluciganov@gmail.com>
2025-04-10 09:37:06 +0700

    Литералы

--- MyName/Program.cs
+++ MyName/Program.cs
@@ -5,10 +5,14 @@ class MainClass
     public static void Main(string[] args)
     {
 
-        string myName;
+        const string myName;
         myName = "Aleks";
 
         Console.WriteLine(myName);
+
+        Console.WriteLine("Привет мир!");
+        Console.WriteLine("Мне 34 года");
+        Console.WriteLine("My name is Aleks");
 
         Console.ReadKey();
     }
