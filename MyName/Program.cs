5aa220bb5e3f80e426da8837af84909f23baa52c
Aleksey Polucyganov <poluciganov@gmail.com>
2025-04-10 09:46:52 +0700

    Управляющие символы

--- MyName/Program.cs
+++ MyName/Program.cs
@@ -5,14 +5,11 @@ class MainClass
     public static void Main(string[] args)
     {
 
-        const string myName;
-        myName = "Aleks";
+        const string myName = "Aleks";
 
         Console.WriteLine(myName);
 
-        Console.WriteLine("Привет мир!");
-        Console.WriteLine("Мне 34 года");
-        Console.WriteLine("My name is Aleks");
+        Console.WriteLine("\t Привет мир! \n \t Мне 34 года \n \t My name is \n Aleks");
 
         Console.ReadKey();
     }
